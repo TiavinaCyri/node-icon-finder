711c6266f651da907bd1fe36be711112816a1e8d
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-02-15 23:52:29 +0300

    .env

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
-const express = require('express');
-const OAuth = require('oauth');
+const express = require("express")
+const OAuth = require("oauth")
+const dotenv = require("dotenv")
+dotenv.config()
 
-const app = express();
+const app = express()
 
-const KEY = '4f3983af446c459ea041fce81810b6f3';
-const SECRET = 'f76d7c6f9fb14724a6551d4d5011874f';
+const KEY = process.env.APP_KEY
+const SECRET = process.env.APP_SECRET
+const URL = process.env.URL
 
-const oauth = new OAuth.OAuth(
-    'https://api.thenounproject.com',
-    'https://api.thenounproject.com',
-    KEY,
-    SECRET,
-    '1.0',
-    null,
-    'HMAC-SHA1'
-);
+const oauth = new OAuth.OAuth(URL, URL, KEY, SECRET, "1.0", null, "HMAC-SHA1")
 
-app.get('/icon/:query', (req, res) => {
-    const query = req.params.query;
+app.get("/icon/:query", (req, res) => {
+    const query = req.params.query
+    console.log(query)
 
-    oauth.get(
-        `https://api.thenounproject.com/v2/icon?query=${query}`,
-        null,
-        null,
-        (err, data, response) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error fetching Noun Project API');
-            } else {
-                res.json(JSON.parse(data));
-            }
+    oauth.get(`https://api.thenounproject.com/v2/icon?query=${query}`, null, null, (err, data) => {
+        if (err) {
+            console.error(err)
+            res.status(500).send("Error fetching Noun Project API")
+        } else {
+            res.json(JSON.parse(data))
         }
-    );
-});
+    })
+})
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}`)
+})
